37597f0860f1b5e88d0fe1ba7df00657301bac27
u3l6 <Liu3l6@outlook.com>
2024-12-28 11:42:54 -0800

    enhance tray icon functionality; add window unminimize and focus actions in main.ts and update capabilities in default.json

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { TrayIcon } from '@tauri-apps/api/tray'
+import { TrayIcon, TrayIconEvent } from '@tauri-apps/api/tray'
 import { defaultWindowIcon } from '@tauri-apps/api/app'
 import { Menu } from '@tauri-apps/api/menu'
 import { exit } from '@tauri-apps/plugin-process'
-import { Window } from '@tauri-apps/api/window'
+import { getCurrentWindow } from '@tauri-apps/api/window'
 
+const window = await getCurrentWindow()
 const icon = (await defaultWindowIcon()) || 'src-tauri\\icons\\icon.ico'
 const menu = await Menu.new({
   items: [
     {
       id: 'open',
       text: 'Open',
-      action: () => {},
+      action: () => {
+        window.unminimize()
+        window.setFocus()
+      },
     },
     {
       id: 'quit',
@@ -27,6 +31,19 @@ const options = {
   icon,
   menu,
   menuOnLeftClick: false,
+  action: (event: TrayIconEvent) => {
+    switch (event.type) {
+      case 'Click':
+        if (event.button == 'Left' && event.buttonState == 'Down') {
+          console.log(
+            `mouse ${event.button} button pressed, state: ${event.buttonState}`
+          )
+          window.unminimize()
+          window.setFocus()
+        }
+        break
+    }
+  },
 }
 
 await TrayIcon.new(options)
