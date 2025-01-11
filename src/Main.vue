1cf5c207998e7dc14026453f2fc91cfe0bd39ec2
u3l6 <Liu3l6@outlook.com>
2025-01-10 21:23:22 -0800

    feat: add Material Symbols font and update window dimensions; implement Menubar for navigation

--- src/Main.vue
+++ src/Main.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-import { Card } from "@/components/ui/card";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
 import { window } from "@/main";
-import { goTo } from "@/plugin/router";
 import { useSettingsStore } from "@/stores/settings";
 import { exit } from "@tauri-apps/plugin-process";
+import Card from "primevue/card";
+import Menubar from "primevue/menubar";
+import { goTo } from "./plugin/router";
 
 const settingsStore = useSettingsStore();
 function close_button() {
@@ -23,6 +17,23 @@ function close_button() {
 function minimize_button() {
   window.minimize();
 }
+
+const menu_items = [
+  {
+    label: "Installation",
+    icon: "install_desktop",
+    command: () => {
+      goTo("/installation");
+    },
+  },
+  {
+    label: "Settings",
+    icon: "settings",
+    command: () => {
+      goTo("/settings");
+    },
+  },
+];
 </script>
 
 <template>
@@ -30,15 +41,17 @@ function minimize_button() {
   <div class="fixed top-0 right-0 h-auto z-50 flex">
     <button class="px-3 py-1.5 hover:bg-[#e9e9e9]" @click="minimize_button()">
       <span
-        v-svg="'minimize'"
-        class="w-5 h-5 overflow-hidden flex items-center justify-center"
-      ></span>
+        class="material-symbols-rounded w-5 h-5 flex items-center justify-center"
+      >
+        remove
+      </span>
     </button>
     <button class="px-3 py-1.5 hover:bg-[#c42b1c]" @click="close_button()">
       <span
-        v-svg="'close'"
-        class="w-5 h-5 overflow-hidden flex items-center justify-center"
-      ></span>
+        class="material-symbols-rounded w-5 h-5 flex items-center justify-center"
+      >
+        close
+      </span>
     </button>
   </div>
 
@@ -48,29 +61,14 @@ function minimize_button() {
       <h1 class="text-lg font-semibold relative pt-3 pl-6 pb-2">AppPorter</h1>
     </div>
 
-    <!-- Navigation -->
-    <div class="px-6 py-1">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              :class="[navigationMenuTriggerStyle(), 'px-4']"
-              @click="goTo('/Installation')"
-            >
-              Installation
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink
-              :class="[navigationMenuTriggerStyle(), 'px-4']"
-              @click="goTo('/Settings')"
-            >
-              Settings
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
+    <Menubar :model="menu_items" class="flex">
+      <template #item="{ item }">
+        <a href="#">
+          <span class="material-symbols-rounded">{{ item.icon }}</span>
+          <span>{{ item.label }}</span>
+        </a>
+      </template>
+    </Menubar>
   </div>
 
   <!-- Main Content -->
