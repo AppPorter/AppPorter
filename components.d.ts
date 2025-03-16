0a47926a10094286e811e8ed9e6033e0ad247fe9
u3l6 <w@u316.org>
2025-03-16 12:26:48 -0700

    feat: remove Button component from global declarations and center window in Tauri configuration

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    Button: typeof import('primevue/button')['default']
     Panel: typeof import('primevue/panel')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
