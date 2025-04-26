09f53029766b707cc6af19da089662403f7509d6
u3l6 <w@u316.org>
2025-04-26 13:32:17 -0700

    refactor: move file_system functions to validation module and update imports

--- src-tauri/src/operations/mod.rs
+++ src-tauri/src/operations/mod.rs
@@ -1,19 +1,19 @@
 pub mod archive;
 pub mod copy_only;
 pub mod download;
-pub mod file_system;
 pub mod get_details;
 pub mod installation;
 pub mod installation_process;
 pub mod launcher;
 pub mod uninstallation;
+pub mod validation;
 
 pub use archive::*;
 pub use copy_only::{copy_only, CopyOnlyConfig};
 pub use download::*;
-pub use file_system::*;
 pub use get_details::*;
 pub use installation::*;
 pub use installation_process::*;
 pub use launcher::*;
 pub use uninstallation::*;
+pub use validation::*;
